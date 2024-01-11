c62ddd708f9fea563debd958beee3f2c80fa4e8a
Sunjay Armstead <connect@sunjayarmstead.local>
2024-01-11 14:56:28 -0500

    Add create_index contents and test

--- site_generator.rb
+++ site_generator.rb
@@ -1,8 +1,9 @@
 class SiteGenerator
-  attr_reader :site_name
+  attr_reader :site_name, :author_name
 
-  def initialize(site_name:, author: '', js_folder: false, css_folder: false)
+  def initialize(site_name:, author_name: '', js_folder: false, css_folder: false)
     @site_name = site_name
+    @author_name = author_name
   end
 
   def create_site_directory
@@ -10,6 +11,22 @@ class SiteGenerator
   end
 
   def create_index
-    IO.write("#{site_name}/index.html")
+    data = build_index_html
+    IO.write("#{site_name}/index.html", data)
+  end
+
+  def build_index_html
+    <<~HEREDOC
+      <html>
+        <head>
+          <title>#{site_name}</title>
+          <meta name="author" content="#{author_name}" />
+        </head>
+        <body>
+          <h1>Welcome to #{site_name}.</h1>
+          <h2>Time to monkey around.</h2>
+        </body>
+      </html>
+    HEREDOC
   end
 end
--- spec/site_generator_spec.rb
+++ spec/site_generator_spec.rb
@@ -1,22 +1,37 @@
 require_relative '../site_generator'
 
 describe SiteGenerator do
+  let(:site_name) { 'awesome site' }
+
   before do
     allow(Dir).to receive(:mkdir).and_return(0)
     allow(IO).to receive(:write)
   end
 
   it 'creates a site directory' do
-    site_name = 'awesome site'
     site_generator = SiteGenerator.new(site_name: site_name)
     site_generator.create_site_directory
     expect(Dir).to have_received(:mkdir).with(site_name)
   end
 
   it 'creates an index.html file in site directory' do
-    site_name = 'awesome site'
     site_generator = SiteGenerator.new(site_name: site_name)
     site_generator.create_index
-    expect(IO).to have_received(:write).with("#{site_name}/index.html")
+    expect(IO).to have_received(:write).with(
+      "#{site_name}/index.html",
+      anything
+    )
+  end
+
+  it 'creates an index.html file with site name and author' do
+    author_name = 'George of teh 🙊 Jungle 🍌'
+    site_generator = SiteGenerator.new(site_name: site_name, author_name: author_name)
+    site_generator.create_index
+    ['html', 'head', 'body', author_name, site_name].each do |fragment|
+      expect(IO).to have_received(:write).with(
+        anything,
+        match(fragment)
+      )
+    end
   end
 end
